101a08b164c2a9f0f9ffb93097450552181f9a69
Ruud Boon <ruud@ruudboon.io>
2020-05-15 15:59:25 +0200

    Changes

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
+import VueNativeSock from 'vue-native-websocket'
 
 Vue.config.productionTip = false
+
+Vue.use(VueNativeSock, 'ws://172.20.0.169:9001')
 
 new Vue({
   vuetify,
